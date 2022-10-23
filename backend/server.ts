e1053fdfdfdf2336de1e3952a973f95b405a916d
sagemaxn <sagemaxn@gmail.com>
2022-10-23 15:23:42 -0400

    yeah

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
 import * as menu from "./menuitems.json";
-import cors from "cors";
+//import * as favicon from 'serve-favicon'
+
 dotenv.config();
 import itemRouter from './controllers/item'
  
@@ -9,9 +10,38 @@ import connect from "./utils/mongo";
 connect();
 
 const app = express();
-app.use(express.static(".next"))
-app.use(cors());
+
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "./build", "index.html"));
+});
+
+
+// app.use("*", async (req: Request, res: Response, next: NextFunction) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://sage-wordle.herokuapp.com/")
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization")
+//   res.setHeader("Access-Control-Allow-Credentials", "true")
+//   // res.setHeader(
+//   //   'Content-Security-Policy',
+//   //   "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+//   // );
+//   next()
+// })
+;
+
+// app.all('*', function (req, res) {
+//   res.status(200).sendFile(`/`, { root: dist });
+// });
+
 app.use('/items', itemRouter)
+
+// app.all('*', function (req, res) {
+//   res.status(200).sendFile(`/`, { root: dist });
+// });
+
 
 const PORT = process.env.PORT || 4000
 
@@ -23,3 +53,4 @@ app.get("/api/menu", (req: Request, res: Response, next: NextFunction) => {
   console.log(menu);
   res.status(200).json(menu);
 });
+
